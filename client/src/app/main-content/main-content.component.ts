7bf9260056fc38207d162e95547f5c9a474a2f21
Rafak <rafalfilak22@gmail.com>
2022-07-05 15:44:44 +0200

    add footer layout. Create animation for caption frame

--- client/src/app/main-content/main-content.component.ts
+++ client/src/app/main-content/main-content.component.ts
@@ -9,19 +9,22 @@ import {BasicServiceService} from '../basic-service.service'
 export class MainContentComponent implements OnInit {
 
   randomData=[];
-  text="asdasdasd"
+  addedText:Array<Object>=[];
+
   checkBoxes = [
     {id:1,select:false,caption:'Opcja pierwsza'},
     {id:2,select:false,caption:'Opcja druga'},
     {id:3,select:false,caption:'Opcja losowa'},
   ];
 
-  selectOption=0;
-
   onInputChange(id:any){
    this.checkBoxes.forEach(item=>{
     if(item.id===id){
-      item.select=true;
+      if(item.select){
+        item.select=true
+      }else if(!item.select){
+        item.select=false;
+      }
     }else item.select=false;
    });
   }
@@ -40,25 +43,34 @@ export class MainContentComponent implements OnInit {
     var textToAdd="";
     const {id} = option;
     if(id===1 || id===2){
-     const {text} = this.randomData[id-1]
+     const text = this.randomData[id-1]
      textToAdd=text;
     }else if(id===3){
       const randomIndex = Math.floor(Math.random()*4+3);
-      const {text} = this.randomData[randomIndex];
-      textToAdd = text
+      const text = this.randomData[randomIndex];
+      textToAdd = text;
     }
     return textToAdd
    }else return; 
   }
 
   
-  replaceText=()=>{
+  replaceText(){
     const text = this.checkTextOptionForIndex();
-    console.log(text)
+    if(text!==undefined){
+      this.addedText.length=0;
+      this.addedText.push(text);
+      console.log(this.addedText)
+    }
   }
 
-  addText=()=>{
-    
+  addText(){
+    const text = this.checkTextOptionForIndex();
+    if(text!==undefined){
+      //sprawdzanie czy dany indeks już znajduje się w tablicy
+      this.addedText.push(text);
+      console.log(this.addedText)
+    }
   }
 
 
