ac65dc662dd3168b0555ac8e62b39e461d34a6d5
Rafak <rafalfilak22@gmail.com>
2022-07-05 17:49:22 +0200

    random commit for safety

--- client/src/app/footer/footer.component.ts
+++ client/src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, transition, animate, style } from '@angular/animations'
 
 @Component({
   selector: 'app-footer',
@@ -7,7 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  buttonImage = []
+  showDialog:boolean = false;
+  arrowDownImg = '../../assets/arrow-down.png'
+  arrowUpImg = '../../assets/arrow-up.png'
   constructor() { }
+
+  
+  onButtonClick(){
+    this.showDialog=!this.showDialog;
+    console.log(this.showDialog)
+  }
+
 
   ngOnInit(): void {
   }
